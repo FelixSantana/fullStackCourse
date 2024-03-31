72ad0742ee45a762af7d2a87216750d7d2a70f75
Felix Santana <flexsantana99@gmail.com>
2024-03-31 17:06:54 -0400

    Adding part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,19 +1,29 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course = {course} />
-      <Content part = {part1} exercises = {exercises1}/>
-      <Content part = {part2} exercises = {exercises2}/>
-      <Content part = {part3} exercises = {exercises3}/>
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <Header course = {course.name} />
+      <Content part1 = {course.parts[0].name} exercises1 = {course.parts[0].exercises}/>
+      <Content part2 = {course.parts[1].name} exercises2 = {course.parts[1].exercises}/>
+      <Content part3 = {course.parts[2].name} exercises3 = {course.parts[2].exercises}/>
+      <Total exercises1 = {course.parts[0].exercises} exercises2 = {course.parts[1].exercises} exercises3 = {course.parts[2].exercises} />
     </div>
   )
 }
@@ -29,14 +39,22 @@ const Header = (props) =>{
 const Content = (props) =>{
   return(
     <div>
+      <Part part = {props.part1} exercises = {props.exercises1}/>
+      <Part part = {props.part2} exercises = {props.exercises2}/>
+      <Part part = {props.part3} exercises = {props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) =>{
+  return(
+    <div>
       <p>
         {props.part} {props.exercises}
       </p>
     </div>
   )
 }
-
-
 //Calculamos el total de ejercicios del curso
 const Total = (props) =>{
   return(
